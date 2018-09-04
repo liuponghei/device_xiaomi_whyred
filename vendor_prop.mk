9246ecc8da9fabab571205b775616afc143b7ea4
liuponghei <liuponghei@users.noreply.github.com>
2018-09-04 15:34:32 +0800

    Update vendor_prop.mk

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -91,11 +91,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapgrowthlimit=512m \
+    dalvik.vm.heapsize=768m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
+    dalvik.vm.heapmaxfree=64m
 
 # Device Specific
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -173,11 +173,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Memory
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapgrowthlimit=512m \
+    dalvik.vm.heapsize=768m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
+    dalvik.vm.heapmaxfree=64m
 
 # Netflix custom property
 PRODUCT_PROPERTY_OVERRIDES += \
