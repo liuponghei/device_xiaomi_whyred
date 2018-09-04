e0b14ac2b3d73989ecff540b250d28c33612bdcc
liuponghei <liuponghei@users.noreply.github.com>
2018-09-04 15:32:54 +0800

    Update treble.mk

--- treble.mk
+++ treble.mk
@@ -52,7 +52,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm660
+    android.hardware.biometrics.fingerprint@2.1-service.asus_sdm660
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
@@ -91,7 +91,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sdm660
+    android.hardware.light@2.0-service.asus_sdm660
 
 # Media
 PRODUCT_PACKAGES += \
